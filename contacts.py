b4cab31f85f5f3efa8709cdf5b415d5d1b3ace18
mateusz-ptak <mptak1709@gmail.com>
2023-02-11 22:42:28 +0100

    added some comments to make the code more readable

--- contacts.py
+++ contacts.py
@@ -13,13 +13,14 @@ class Controller:
     def __init__(self):
         self.contacts = []
 
+    # Adding a contact
     def addContact(self, firstName, secondName):
         contact = Contact(firstName, secondName)
         self.contacts.append(contact)
         print(f"Added {firstName} {secondName} to the contact list")
         self.save()
 
-
+    # Showing all contacts
     def showContacts(self):
         for contact in self.contacts:
             print(f"First Name: {contact.firstName}\nSecond Name: {contact.secondName}\nPhone Numbers:")
@@ -27,22 +28,25 @@ class Controller:
                 print(f"\t{i+1}: {phoneNumber}")
             print()
 
+    # Adding a phone number to the selected contact
     def addPhoneNumber(self, contact, phoneNumber):
         contact.phoneNumbers.append(phoneNumber)
         print(f"Added {phoneNumber} to the list of {contact.firstName} {contact.secondName}'s phone numbers")
         self.save()
 
+    # Deleting a contact
     def deleteContact(self, contact):
         self.contacts.remove(contact)
         print(f"Deleted {contact.firstName} {contact.secondName} from the contact list")
         self.save()
 
+    # Deleting a phone number from the selected contact
     def deletePhoneNumber(self, contact, phoneNumber):
         contact.phoneNumbers.remove(phoneNumber)
         print(f"Deleted {phoneNumber} from the list of {contact.firstName} {contact.secondName}'s phone numbers")
         self.save()
 
-
+    # Saving data to binary file "data.dat"
     def save(self):
         f = open("data.dat", "wb")
         pickle.dump(self.contacts, f)
@@ -55,6 +59,7 @@ class App(Controller):
 
     def __init__(self):
         super().__init__()
+        # Connecting to the binary file "data.dat", if it's not found - creating a new one
         try:
             f = open("data.dat", "rb")
             self.contacts = pickle.load(f)
@@ -64,6 +69,7 @@ class App(Controller):
             pickle.dump([], f)
             f.close()
 
+    # Menu that shows up when you start the app
     def menu(self):
         menu = "1 - Add a Contact, 2 - Show Contacts, 3 - Add a Phone Number, 4 - Delete a Contact, 5 - Delete a Phone Number, 6 - Exit"
         while True:
@@ -116,6 +122,7 @@ class App(Controller):
             else:
                 print("I don't understand")
 
+    # Finding a contact in contacts by matching the secondName values
     def find(self, secondName):
         for contact in self.contacts:
             if contact.secondName == secondName:
