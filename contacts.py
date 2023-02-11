ec19d7c16bb60e746fc40c7d57a02c6c9f2901b7
mateusz-ptak <mptak1709@gmail.com>
2023-02-11 20:09:57 +0100

    created a phonebook

---
+++ contacts.py
@@ -0,0 +1,129 @@
+import pickle
+
+class Contact:
+
+    def __init__(self, firstName, secondName):
+        self.firstName = firstName
+        self.secondName = secondName
+        self.phoneNumbers = []
+
+
+class Controller:
+
+    def __init__(self):
+        self.contacts = []
+
+    def addContact(self, firstName, secondName):
+        contact = Contact(firstName, secondName)
+        self.contacts.append(contact)
+        print(f"Added {firstName} {secondName} to the contact list")
+        self.save()
+
+
+    def showContacts(self):
+        for contact in self.contacts:
+            print(f"First Name: {contact.firstName}\nSecond Name: {contact.secondName}\nPhone Numbers:")
+            for i, phoneNumber in enumerate(contact.phoneNumbers):
+                print(f"\t{i+1}: {phoneNumber}")
+            print()
+
+    def addPhoneNumber(self, contact, phoneNumber):
+        contact.phoneNumbers.append(phoneNumber)
+        print(f"Added {phoneNumber} to the list of {contact.firstName} {contact.secondName}'s phone numbers")
+        self.save()
+
+    def deleteContact(self, contact):
+        self.contacts.remove(contact)
+        print(f"Deleted {contact.firstName} {contact.secondName} from the contact list")
+        self.save()
+
+    def deletePhoneNumber(self, contact, phoneNumber):
+        contact.phoneNumbers.remove(phoneNumber)
+        print(f"Deleted {phoneNumber} from the list of {contact.firstName} {contact.secondName}'s phone numbers")
+        self.save()
+
+
+    def save(self):
+        f = open("data.dat", "wb")
+        pickle.dump(self.contacts, f)
+        f.close()
+
+
+
+
+class App(Controller):
+
+    def __init__(self):
+        super().__init__()
+        try:
+            f = open("data.dat", "rb")
+            self.contacts = pickle.load(f)
+            f.close()
+        except:
+            f = open("data.dat", "wb")
+            pickle.dump([], f)
+            f.close()
+
+    def menu(self):
+        menu = "1 - Add a Contact, 2 - Show Contacts, 3 - Add a Phone Number, 4 - Delete a Contact, 5 - Delete a Phone Number, 6 - Exit"
+        while True:
+            print(menu)
+            prompt = input(">> ")
+
+            if prompt == "1":
+
+                firstName = input("First Name: ")
+                secondName = input("Second Name: ")
+                self.addContact(firstName, secondName)
+
+            elif prompt == "2":
+
+                self.showContacts()
+
+            elif prompt == "3":
+
+                secondName = input("Second Name of the contact: ")
+                contact = self.find(secondName)
+                if contact:
+                    phoneNumber = input("Phone Number: ")
+                    self.addPhoneNumber(contact, phoneNumber)
+
+            elif prompt == "4":
+
+                secondName = input("Second Name of the contact: ")
+                contact = self.find(secondName)
+                if contact:
+                    self.deleteContact(contact)
+
+            elif prompt == "5":
+
+                secondName = input("Second Name of the contact: ")
+                contact = self.find(secondName)
+                if contact:
+                    for i, phoneNumber in enumerate(contact.phoneNumbers):
+                        print(f"{i}: {phoneNumber}")
+
+                    indeks = input("Index of the phone number: ")
+
+                    try:
+                        phoneNumber = contact.phoneNumbers[int(indeks)]
+                        self.deletePhoneNumber(contact, phoneNumber)
+                    except:
+                        print("Invalid index")
+
+            elif prompt == "6":
+                break
+            else:
+                print("I don't understand")
+
+    def find(self, secondName):
+        for contact in self.contacts:
+            if contact.secondName == secondName:
+                return contact
+        print(f"Couldn't find a person named {secondName}")
+        return 0
+
+
+
+app = App()
+app.menu()
